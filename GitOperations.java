c3a93b1658c9bb3a94e4654fb98a87544cf92720
ajitj1234 <ajitjagtap2008@gmail.com>
2025-03-12 17:07:36 +0530

    rate

---
+++ GitOperations.java
@@ -0,0 +1,11 @@
+package Intro;
+
+public class GitOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First git operation");
+		System.out.println("second git");
+	}
+
+}
